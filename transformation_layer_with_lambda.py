5d7a4a2eebf7b24cd9e378cdff6225427c73830f
Sachin Goyal <131760723+MrSachinGoyal@users.noreply.github.com>
2024-05-15 10:17:59 +0530

    Update transformation_layer_with_lambda.py

--- transformation_layer_with_lambda.py
+++ transformation_layer_with_lambda.py
@@ -1,8 +1,10 @@
 import base64
 import json
-from datetime import datetime
+import pandas as pd
 
 def lambda_handler(event, context):
+    print("Event is", event)
+
     output_records = []
 
     for record in event['records']:
@@ -12,6 +14,21 @@ def lambda_handler(event, context):
             payload = base64.b64decode(record['data'])
             payload_json = json.loads(payload)
 
+            print("Payload_Json or Deserialised data is", payload_json)
+
+            # Access the 'eventName' and 'ApproximateCreationDateTime' from the payload
+            event_name = payload_json['eventName']
+            approx_creation_datetime = payload_json['dynamodb']['ApproximateCreationDateTime']
+            
+            # Convert the Unix timestamp to a human-readable date format
+            # creation_datetime = datetime.utcfromtimestamp(approx_creation_datetime).isoformat() + 'Z'
+    
+            # Convert the Unix timestamp to a pandas datetime object
+            creation_datetime = pd.to_datetime(approx_creation_datetime, unit='s', utc=True)
+            
+            # # Convert the pandas datetime to a formatted string
+            creation_datetime_str = creation_datetime.isoformat() + 'Z'
+
             # Access the data in the 'dynamodb' key
             dynamodb_data = payload_json['dynamodb']
             new_image = dynamodb_data['NewImage']
@@ -19,12 +36,14 @@ def lambda_handler(event, context):
 
             # Extract required fields from NewImage
             transformed_data = {
-                'orderid': new_image['orderid']['S'],
+                'order_id': new_image['order_id']['S'],
                 'product_name': new_image['product_name']['S'],
                 'quantity': int(new_image['quantity']['N']),
                 'price': float(new_image['price']['N']),
+                'cdc_event_type': event_name,  # Include the event name
+                'creation_datetime': creation_datetime_str  # Include the creation datetime
             }
-
+            
             # Convert the transformed data to a JSON string and then encode it as base64
             transformed_data_str = json.dumps(transformed_data) + '\n'
             transformed_data_encoded = base64.b64encode(transformed_data_str.encode('utf-8')).decode('utf-8')
